a5e231ca53857b43f123d81c1bc08c1e4e5327e5
Serhii Svystunov <snsvistunov@gmail.com>
2022-07-07 09:52:11 +0300

    Resolve comments

--- main.go
+++ main.go
@@ -12,43 +12,61 @@ const cowFeedPerKg int = 25
 
 type FarmAnimal interface {
 	feedWeightPerMonth() int
-	String()
-}
-
-//Basic animal struct
-type Animal struct {
-	Weight    int
-	FeedPerKg int
-	Name      string
-}
-
-func (animal Animal) feedWeightPerMonth() int {
-	return animal.Weight * animal.FeedPerKg
-}
-
-func (animal Animal) String() {
-	fmt.Printf("Me name is %s. My weight is %d kilos. I need %d kilos of feed per month.\n", animal.Name, animal.Weight, animal.feedWeightPerMonth())
+	fmt.Stringer
 }
 
 type Cat struct {
-	Animal
+	CatWeight    int
+	CatFeedPerKg int
+	CatName      string
+}
+
+func (cat Cat) feedWeightPerMonth() int {
+	return cat.CatWeight * catFeedPerKg
+}
+
+func (cat Cat) String() string {
+	return fmt.Sprintf("My name is %s. My weight is %d kilos. I need %d kilos of feed per month.", cat.CatName, cat.CatWeight, cat.feedWeightPerMonth())
 }
 
 type Dog struct {
-	Animal
+	DogWeight    int
+	DogFeedPerKg int
+	DogName      string
+}
+
+func (dog Dog) feedWeightPerMonth() int {
+	return dog.DogWeight * dog.DogFeedPerKg
+}
+
+func (dog Dog) String() string {
+	return fmt.Sprintf("My name is %s. My weight is %d kilos. I need %d kilos of feed per month.", dog.DogName, dog.DogWeight, dog.feedWeightPerMonth())
 }
 
 type Cow struct {
-	Animal
+	CowWeight    int
+	CowFeedPerKg int
+	CowName      string
 }
 
-func typeFarmInfo(farm []FarmAnimal) (totalFarmFeedPerMonth int) {
+func (cow Cow) feedWeightPerMonth() int {
+	return cow.CowWeight * cow.CowFeedPerKg
+}
+
+func (cow Cow) String() string {
+	return fmt.Sprintf("My name is %s. My weight is %d kilos. I need %d kilos of feed per month.", cow.CowName, cow.CowWeight, cow.feedWeightPerMonth())
+}
+
+func typeFarmInfo(farm []FarmAnimal) int {
+
+	totalFarmFeedPerMonth := 0
 
 	for _, a := range farm {
-		a.String()
+		fmt.Println(a.String())
 		totalFarmFeedPerMonth += a.feedWeightPerMonth()
 	}
-	return
+
+	return totalFarmFeedPerMonth
 }
 
 //A function that creates animals on the farm. n - total number of each animals on the farm
@@ -75,15 +93,15 @@ func makeFarm(n int) (farm []FarmAnimal) {
 		switch newRandomAnimal {
 		case 1:
 			newCatWeight := rand.Intn(maxCatWeight-minCatWeight) + minCatWeight
-			animal = Cat{Animal{Weight: newCatWeight, FeedPerKg: catFeedPerKg, Name: "Cat"}}
+			animal = Cat{newCatWeight, catFeedPerKg, "Cat"}
 
 		case 2:
 			newDogWeight := rand.Intn(maxDogWeight-minDogWeight) + minDogWeight
-			animal = Dog{Animal{Weight: newDogWeight, FeedPerKg: dogFeedPerKg, Name: "Dog"}}
+			animal = Dog{newDogWeight, dogFeedPerKg, "Dog"}
 
 		case 3:
 			newCowWeight := rand.Intn(maxCowWeight-minCowWeight) + minCowWeight
-			animal = Cow{Animal{Weight: newCowWeight, FeedPerKg: cowFeedPerKg, Name: "Cow"}}
+			animal = Cow{newCowWeight, cowFeedPerKg, "Cow"}
 
 		}
 		farm = append(farm, animal)
@@ -93,7 +111,7 @@ func makeFarm(n int) (farm []FarmAnimal) {
 }
 
 func main() {
-	maxNumberOfAnimals := 15                                                          //Any value can be specified.
-	farm := makeFarm(maxNumberOfAnimals)                                              //Making new farm with random num of cats, dogs and cows
-	fmt.Printf("The farm needs %d kilograms of feed per month\n", typeFarmInfo(farm)) //Printing information about farm
+	maxNumberOfAnimals := 15                                                           //Any value can be specified.
+	farm := makeFarm(maxNumberOfAnimals)                                               //Making new farm with random num of cats, dogs and cows
+	fmt.Printf("The farm needs %d kilograms of feed per month.\n", typeFarmInfo(farm)) //Printing information about farm
 }
